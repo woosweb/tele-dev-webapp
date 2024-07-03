08557224b97250e0385adf667c65d63180651c04
hwkim <hwkim@zenaad.com>
2024-07-03 17:23:28 +0900

    feat: settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: "/batching-ai-tele-webapp"
+};
 
 export default nextConfig;
