2eaa7ca162e6f801c3be5ebd8ee5be53d1f8fdf4
hwkim <hwkim@zenaad.com>
2024-07-03 17:43:25 +0900

    feat: assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: "/tele-dev-webapp"
+    basePath: "/tele-dev-webapp",
+    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://woosweb.github.io/tele-dev-webapp/' : ''
 };
 
 export default nextConfig;
