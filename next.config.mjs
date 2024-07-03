caea81b6defeaaa81121d37f696155ebaaabdde8
hwkim <hwkim@zenaad.com>
2024-07-03 17:49:27 +0900

    feat: images

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@
 const nextConfig = {
     output: 'export',
     basePath: "/tele-dev-webapp",
-    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://woosweb.github.io/tele-dev-webapp/' : ''
+    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://woosweb.github.io/tele-dev-webapp/' : '',
+    images: {
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'woosweb.github.io',
+            }
+        ],
+    }
 };
 
 export default nextConfig;
